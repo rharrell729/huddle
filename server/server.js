6e296f39d3b82c073117ece66d46399024bbbb49
evanboyle <epb5te@virginia.edu>
2014-12-14 18:16:21 -0800

    now using express

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+
+app.get('/foo/', function (req, res) {
+  res.send('Hello foo')
+})
+
+app.listen(3000)
